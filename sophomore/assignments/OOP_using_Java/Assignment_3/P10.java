168d29827593a33d3d780a9d18a1f65415200303
Mk44 <57295745+HulkBusterMark44@users.noreply.github.com>
2022-09-14 21:23:33 +0530

    Update P10.java

--- sophomore/assignments/OOP_using_Java/Assignment_3/P10.java
+++ sophomore/assignments/OOP_using_Java/Assignment_3/P10.java
@@ -128,3 +128,82 @@ class Cone
     }
 }
 
+//In Scanner class
+
+import java.util.*;
+public class Ten
+{
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        Box b=new Box();                 
+        Cube cu=new Cube();                  
+        Cylinder cr=new Cylinder();
+        Cone co=new Cone(); 
+        
+        System.out.println("1. Box");             
+        System.out.println("2. Cube");
+        System.out.println("3. Cylinder");
+        System.out.println("4. Cone");
+ 
+        System.out.println("enter your choice:");      
+        int a= sc.nextInt();
+ 
+        switch (a)                     
+        {
+            case 1:
+                System.out.println("Enter value for length:");
+                float length = sc.nextFloat();
+     
+                System.out.println("Enter value for breadth:");
+                float breadth= sc.nextFloat();
+
+                System.out.println("Enter value for height:");
+                float height= sc.nextFloat();
+                
+                System.out.println("The surface area for box is=" + b.surfaceArea(length,breadth,height));
+                System.out.println("The volume of box=" + b.volume(length,breadth,height));
+ 
+                break;
+ 
+            case 2:
+                System.out.println("Enter value for side:");
+                Float side= sc.nextFloat();;
+ 
+                System.out.println("The surface area for cube is=" + cu.surfaceArea(side));
+                System.out.println("The volume of cube=" + cu.volume(side));
+                break;
+
+            case 3:
+ 
+                System.out.println("Enter value for radius:");
+                float radius= sc.nextFloat();;
+ 
+                System.out.println("Enter value for height:");
+                float height1= sc.nextFloat();;
+ 
+                System.out.println("The surface area for cylinder is=" + cr.surfaceArea(radius,height1));
+                System.out.println("The volume of cylinder is=" + cr.volume(radius,height1));
+                break;
+ 
+            case 4:
+ 
+                System.out.println("Enter value for radius:");
+                float radius1= sc.nextFloat();;
+ 
+                System.out.println("Enter value for height:");
+                float height2= sc.nextFloat();;
+ 
+                System.out.println("The surface area for cone is=" + co.surfaceArea(radius1,height2));
+                System.out.println("The volume of cone is=" + co.volume(radius1,height2));
+                break;
+ 
+            case 5:
+                System.exit(0);
+ 
+            default:
+                System.out.println("Invalid Entry!");
+        }
+    }
+}
